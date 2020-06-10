0b31b24fb0b2483457bbe9ceb5a7d863c79adaa6
30Massas <luis.vieira36@gmail.com>
2020-06-10 20:46:43 +0100

    Views e jar, bug fixed on load.

--- src/Views/VoluntarioView.java
+++ src/Views/VoluntarioView.java
@@ -7,6 +7,19 @@ public class VoluntarioView implements TrazAquiView {
      */
     @Override
     public void show() {
+        System.out.println("                            ███╗   ███╗███████╗███╗   ██╗██╗   ██╗                                            \n" +
+                "                           ████╗ ████║██╔════╝████╗  ██║██║   ██║                                            \n" +
+                "                           ██╔████╔██║█████╗  ██╔██╗ ██║██║   ██║                                            \n" +
+                "                           ██║╚██╔╝██║██╔══╝  ██║╚██╗██║██║   ██║                                            \n" +
+                "                           ██║ ╚═╝ ██║███████╗██║ ╚████║╚██████╔╝                                            \n" +
+                "                           ╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝                                             \n" +
+                "                                                                                  \n" +
+                "██╗   ██╗ ██████╗ ██╗     ██╗   ██╗███╗   ██╗████████╗ █████╗ ██████╗ ██╗ ██████╗ \n" +
+                "██║   ██║██╔═══██╗██║     ██║   ██║████╗  ██║╚══██╔══╝██╔══██╗██╔══██╗██║██╔═══██╗\n" +
+                "██║   ██║██║   ██║██║     ██║   ██║██╔██╗ ██║   ██║   ███████║██████╔╝██║██║   ██║\n" +
+                "╚██╗ ██╔╝██║   ██║██║     ██║   ██║██║╚██╗██║   ██║   ██╔══██║██╔══██╗██║██║   ██║\n" +
+                " ╚████╔╝ ╚██████╔╝███████╗╚██████╔╝██║ ╚████║   ██║   ██║  ██║██║  ██║██║╚██████╔╝\n" +
+                "  ╚═══╝   ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝ ╚═════╝ \n");
         System.out.println("1 -> Escolher encomenda para entregar");
         System.out.println("2 -> Consultar encomendas do Sistema para entregar");
         System.out.println("3 -> Alterar disponibilidade");
@@ -22,14 +35,30 @@ public class VoluntarioView implements TrazAquiView {
     public void show(Object o) {
         if (o instanceof String) System.out.print((String) o );
         else if (o instanceof Boolean){
-            System.out.print("                                      Disponibilidade : ");
+            System.out.println("                     ███╗   ███╗███████╗███╗   ██╗██╗   ██╗                                            \n" +
+                    "                     ████╗ ████║██╔════╝████╗  ██║██║   ██║                                            \n" +
+                    "                     ██╔████╔██║█████╗  ██╔██╗ ██║██║   ██║                                            \n" +
+                    "                     ██║╚██╔╝██║██╔══╝  ██║╚██╗██║██║   ██║                                            \n" +
+                    "                     ██║ ╚═╝ ██║███████╗██║ ╚████║╚██████╔╝                                            \n" +
+                    "                     ╚═╝     ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝                                             \n" +
+                    "                                                                                  \n" +
+                    "██╗   ██╗ ██████╗ ██╗     ██╗   ██╗███╗   ██╗████████╗ █████╗ ██████╗ ██╗ ██████╗ \n" +
+                    "██║   ██║██╔═══██╗██║     ██║   ██║████╗  ██║╚══██╔══╝██╔══██╗██╔══██╗██║██╔═══██╗\n" +
+                    "██║   ██║██║   ██║██║     ██║   ██║██╔██╗ ██║   ██║   ███████║██████╔╝██║██║   ██║\n" +
+                    "╚██╗ ██╔╝██║   ██║██║     ██║   ██║██║╚██╗██║   ██║   ██╔══██║██╔══██╗██║██║   ██║\n" +
+                    " ╚████╔╝ ╚██████╔╝███████╗╚██████╔╝██║ ╚████║   ██║   ██║  ██║██║  ██║██║╚██████╔╝\n" +
+                    "  ╚═══╝   ╚═════╝ ╚══════╝ ╚═════╝ ╚═╝  ╚═══╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝ ╚═════╝ \n");
+            System.out.print("                          Disponibilidade : ");
             if((boolean)o)System.out.println("Disponível");
             else System.out.println("Não Disponível");
-            System.out.println("1 -> Escolher encomenda para entregar");
-            System.out.println("2 -> Consultar encomendas do Sistema para entregar");
-            System.out.println("3 -> Alterar disponibilidade");
-            System.out.println("4 -> Mostrar classificação");
-            System.out.println("S -> Logout");
+            System.out.println("            ┌──────────────────────────────────────────────────────┐");
+            System.out.println("            |  1 -> Escolher encomenda para entregar               |");
+            System.out.println("            |  2 -> Consultar encomendas do Sistema para entregar  |");
+            System.out.println("            |  3 -> Alterar disponibilidade                        |");
+            System.out.println("            |  4 -> Mostrar classificação                          |");
+            System.out.println("            |  S -> Logout                                         |");
+            System.out.println("            └──────────────────────────────────────────────────────┘");
+            System.out.print("Option: ");
         }
     }
 }
